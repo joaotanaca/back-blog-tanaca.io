ad33301f223be6bb98b1c0425be0e06a23029de4
Tanaca <jtanacar@gmail.com>
2024-02-23 21:07:09 -0300

    config: typeorm and DB container docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import config from './configs/typeorm.config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { ArticleModule } from './article/article.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            load: [config],
+        }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) =>
+                configService.get('typeorm'),
+        }),
+        ArticleModule,
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
